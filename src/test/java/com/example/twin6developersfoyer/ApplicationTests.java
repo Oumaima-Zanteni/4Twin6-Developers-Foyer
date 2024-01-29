fbbd6c4877739872cc98136ccf9d93651fbf9e16
oumaima <oumaimazanteni15@gmail.com>
2024-01-29 12:11:23 +0100

    first

---
+++ src/test/java/com/example/twin6developersfoyer/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twin6developersfoyer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
